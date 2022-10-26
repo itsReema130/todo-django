f49bb7ae149f18a1ac9d2eff9e2b1fc7b828909d
Reema <reema.sh2211@gmail.com>
2022-10-26 13:37:11 +0300

    adding template and styling

--- todo/settings.py
+++ todo/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
 
-env = environ.Env()
-environ.Env.read_env()
+import os   
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY =os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings.local")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -34,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'todolist',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
