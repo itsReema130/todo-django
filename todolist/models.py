5810ddcd897e5edd86720b5a285b59ce52d72211
Reema <reema.sh2211@gmail.com>
2022-10-26 13:42:26 +0300

    model adding

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class todo (models.Model):
+     text=models.models.CharField( max_length=50)
+     complete=models.BooleanField(default=False)
+     
+     
+     def __str__(self):
+         return self.text
+     
